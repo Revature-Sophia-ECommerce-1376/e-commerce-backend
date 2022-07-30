54ca1f7fbd801074da5ead3898db04826af922dd
Travis <travistaylor.email@gmail.com>
2022-07-30 18:19:45 -0400

    Updated tests to use required args constructors

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -32,7 +32,7 @@ class UserServiceTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		this.dummyUser = new User(1, "dummy.admin@revature.com", "asdf", "Dummy", "User", "Admin", null, null, null );
+		this.dummyUser = new User("dummy.admin@revature.com", "asdf", "Dummy", "User", "Admin");
 	}
 
 	@AfterEach
