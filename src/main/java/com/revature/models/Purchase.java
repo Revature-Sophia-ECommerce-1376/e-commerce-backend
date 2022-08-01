76f410fe61e5765693a1c0273b164eecf72e80b2
Hector Orozco <hect05661@gmail.com>
2022-08-01 16:16:40 -0700

    Reverted change to Purchase model

--- src/main/java/com/revature/models/Purchase.java
+++ src/main/java/com/revature/models/Purchase.java
@@ -42,4 +42,13 @@ public class Purchase {
 	@BusinessKey
     @Column(name = "quantity")
     private int quantity;
+	
+	@Override
+    public String toString() { return BusinessIdentity.toString(this); }
+
+    @Override
+    public boolean equals(final Object o) { return BusinessIdentity.areEqual(this, o); }
+
+    @Override
+    public int hashCode() { return BusinessIdentity.getHashCode(this); }
 }
